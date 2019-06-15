c17ae1e989f78a94676a66ebc22cad81de3875dc
Thiago Cordeiro <thiago.cordeiro@check24.de>
2019-06-15 23:08:13 +0200

    setup phpunit

---
+++ tests/Integration/IntegrationKernel.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\Integration;
+
+use App\Framework\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class IntegrationKernel extends Kernel
+{
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+    {
+        parent::configureContainer($container, $loader);
+
+        $loader->load(__DIR__.'/../Fixtures/*.yaml', 'glob');
+    }
+}
